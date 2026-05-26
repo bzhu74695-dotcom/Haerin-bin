99ccb7f5ed414afe6b3d71e19ee91899b5e2098a
Iccecat_ovo <bzhu74695@gmail.com>
2026-05-27 00:50:41 +0800

    Set GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Haerin/',
+  base: '/Haerin-bin/',
   plugins: [react()],
   optimizeDeps: {
     include: ['gsap', 'gsap/ScrollTrigger'],
